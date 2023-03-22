9d425a2a0c85b28aa8bdf502df6cf56c92e9d635
unknown <aelrawy@gmail.com>
2023-03-22 09:15:04 +0200

    delete unused routs

--- src/app/features/do-box/do-box-routing.module.ts
+++ src/app/features/do-box/do-box-routing.module.ts
@@ -1,21 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from '@app/shared/components/layout/layout.component';
-import { MainLayoutComponent } from '@app/shared/components/main-layout/main-layout.component';
 import { NotFoundComponent } from '@app/shared/components/not-found/not-found.component';
 
 let children: Routes;
 children = [
-  {
-    path: '',
-    component: NotFoundComponent,
-    title: 'Products'
-  },
-  {
-    path: 'add-product',
-    component: NotFoundComponent,
-    title: 'Add product'
-  },
   {
     path: '**',
     component: NotFoundComponent,
